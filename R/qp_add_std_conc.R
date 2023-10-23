dfe312a724ca5c8ffbecfb1d67951f0e8ef928d3
Kai Aragaki <adamaragaki@gmail.com>
2023-10-23 11:12:44 -0400

    abstract concentration addition, export

---
+++ R/qp_add_std_conc.R
@@ -0,0 +1,24 @@
+#' Add known concentrations of protein to standard samples
+#' @param x A `data.frame` containing a `sample_type` and `index` columns.
+#' See details.
+#' @param standard_scale A numeric vector giving the concentrations of the
+#' standards. The units are arbitrary, but will determine the units of the
+#' output concentrations.
+#' @details Input is expected to have two columns:
+#' - `sample_type`: A character vector denoting which samples are standards
+#' with "standard". All other values will be considered unknowns.
+#' - `index`: A numeric column denoting the sample number. Index 1 will
+#' correspond to the first item in `standard_scale`, 2 will be the second, etc.
+#' @return Same type as x, with a `.conc` column
+#' @export
+qp_add_std_conc <- function(x, standard_scale = c(0, 2^((1:7) - 5))) {
+  unk <- x[which(x$sample_type != "standard"), ]
+  unk$.conc <- NA_real_
+  std <- x[which(x$sample_type == "standard"), ]
+  std$.conc <- standard_scale[std$index]
+  rbind(std, unk)
+}
+
+# TODO should probably add standard scale arg to qp too, where magic
+# number 7's length is based on the lenth of standard scale
+# TODO tests
