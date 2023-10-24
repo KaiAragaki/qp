23c76bf58a77f97112f00e74255edf9539bb6dc7
Kai Aragaki <adamaragaki@gmail.com>
2023-10-24 09:37:52 -0400

    testing

---
+++ tests/testthat/test-qc_calc_abs_mean.R
@@ -0,0 +1,32 @@
+test_that("Correct number suspects marked", {
+  expect_true(any(mark_suspect(c(1, 1, 1))))
+  # Will not mark suspects on fewer than 3 samples
+  expect_true(!any(mark_suspect(c(1, 1))))
+  expect_true(!any(mark_suspect(c(1))))
+  # Ignore NAs
+  expect_true(!any(mark_suspect(c(1, NA))))
+  expect_true(!any(mark_suspect(c(1, NA, NA))))
+  expect_true(!any(mark_suspect(c(1, 1, NA))))
+  expect_true(any(mark_suspect(c(1, 1, 1, NA))))
+})
+
+test_that("Outliers marked", {
+  ins <- 1:2
+  # Boundary
+  expect_equal(
+    mark_outlier(c(ins, mean(ins) + sd(ins) * 3)),
+    c(FALSE, FALSE, FALSE)
+  )
+  expect_equal(
+    mark_outlier(c(ins, (mean(ins) + sd(ins) * 3) + 0.001)),
+    c(FALSE, FALSE, TRUE)
+  )
+  expect_equal(mark_outlier(c(1, 1000)), c(FALSE, FALSE))
+  expect_equal(mark_outlier(c(1, 1, 1)), c(FALSE, FALSE, FALSE))
+})
+
+test_that("Outliers ignored when not checked", {
+  vals <- c(1, 1, 100)
+  df <- data.frame(sample_type = "standard", index = 1, value = vals)
+  expect_equal(calc_mean(df, FALSE), rep(mean(vals), length(vals)))
+})
