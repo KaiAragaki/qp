a95cbd8c088d9766f6cc38248ce4b73600523283
Kai Aragaki <adamaragaki@gmail.com>
2023-10-24 08:27:13 -0400

    finish test suite

--- tests/testthat/test-qp_add_std_conc.R
+++ tests/testthat/test-qp_add_std_conc.R
@@ -7,12 +7,12 @@ test_that("index matches order", {
 })
 
 test_that("standard and scale length match", {
-  x <- data.frame(sample_type = "standard", index = 1)
-  expect_error()
+  x <- data.frame(sample_type = "standard", index = 1:2)
+  y <- data.frame(sample_type = "standard", index = 2:3)
+  expect_error(qp_add_std_conc(x, standard_scale = 1))
+  expect_warning(qp_add_std_conc(x, standard_scale = 1:3))
+  expect_error(qp_add_std_conc(y, standard_scale = 1:2))
 })
-
-# Should also check to make sure 2:5 doesn't work with a standard_scale
-# of length 4
 
 test_that("fails on malformed scale", {
   expect_error(check_scale(c(-1, 1, 10)))
@@ -22,8 +22,4 @@ test_that("succeeds when no unknowns provided", {
   x <- data.frame(sample_type = "standard", index = 1)
   a <- qp_add_std_conc(x, standard_scale = 1)
   expect_no_error(a)
-})
-
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
 })
