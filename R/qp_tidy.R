42157ddac55e9f2fb0c7fe1ff85944cdef9eeee0
Kai Aragaki <adamaragaki@gmail.com>
2023-10-25 13:57:06 -0400

    NA values for index are ok

--- R/qp_tidy.R
+++ R/qp_tidy.R
@@ -115,6 +115,8 @@ check_validity <- function(x) {
 
   if (any(x$.abs < 0)) rlang::warn("Negative values found in .abs")
 
-  if (any(is.na(c(x$sample_type, x$index, x$.abs))))
-    rlang::warn("NA values found in `sample_type`, `index`, and/or `.abs`")
+  if (any(is.na(c(x$sample_type, x$.abs))))
+    # index can have NA values if there isn't enough space for a
+    # whole replicate
+    rlang::warn("NA values found in `sample_type` and/or `.abs`")
 }
