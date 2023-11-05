3625ff1bf3c10048ddf41bac502532dee7355f7b
Kai Aragaki <adamaragaki@gmail.com>
2023-11-04 22:53:45 -0400

    test: data.frame input for `qp_calc_conc`

--- tests/testthat/test-qp_calc_conc.R
+++ tests/testthat/test-qp_calc_conc.R
@@ -5,3 +5,11 @@ test_that("concentration calculation is correct", {
   expect_equal(pred_manual, pred_qp$qp$.pred)
   expect_equal(2^pred_manual - 0.5, pred_qp$qp$.pred_conc)
 })
+
+test_that("data.frame forces fit", {
+  fit <- absorbances[1:40, ] |> qp_add_std_conc()
+  expect_warning(
+    qp_calc_conc(fit), "`x` is a `data.frame`, not a `list`"
+  )
+  expect_true(is.list(suppressWarnings(qp_calc_conc(fit))))
+})
