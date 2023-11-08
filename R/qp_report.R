f9567f48868bf1416f89b1b2ae51ce811a8405cc
Kai Aragaki <adamaragaki@gmail.com>
2023-11-07 19:32:05 -0500

    feat: continue report work

--- R/qp_report.R
+++ R/qp_report.R
@@ -4,65 +4,92 @@ qp_report <- function(qp) {
 
 # Standards ----
 ## All ----
-qp_standards_all <- function(qp) {
+qp_standards_all <- function(x) {
   UseMethod("qp_standards_all")
 }
 
-qp_standards_all.data.frame <- function(qp) {
-  dplyr::filter(qp$qp, .data$sample_type == "standard")
+#' @export
+qp_standards_all.data.frame <- function(x) {
+  dplyr::filter(x, .data$sample_type == "standard") |>
+    dplyr::select(
+      Name = ".sample_name",
+      Absorbance = ".abs",
+      "Known Conc." = ".conc",
+      "Predicted Conc." = ".pred_conc",
+      "Mean Predicted Conc." = ".pred_conc_mean",
+      Outlier = ".is_outlier",
+      Row = ".row",
+      Col = ".col"
+    )
 }
 
-qp_standards_all.list <- function(qp) {
-  qp$qp <- qp_standards_all.data.frame(qp$qp)
-  qp
+#' @export
+qp_standards_all.list <- function(x) {
+  x$qp <- qp_standards_all.data.frame(x$qp)
+  x
 }
 
 ## Summary ----
-qp_standards_summary <- function(qp) {
+qp_standards_summary <- function(x) {
   UseMethod("qp_standards_summary")
 }
 
-qp_standards_summary.data.frame <- function(qp) {
-  qp_standards_all(qp) |>
-    dplyr::select(
-      Name = .data$.sample_name,
-      `Known Conc.` = .data$.conc,
-      `Mean Predicted Conc.` = .data$.pred_conc_mean
-    ) |>
-    dplyr::summarise(.by = c("Name", "Known Conc.", "Mean Predicted Conc."))
+#' @export
+qp_standards_summary.data.frame <- function(x) {
+  qp_standards_all(x) |>
+    dplyr::summarize(.by = c("Name", "Known Conc.", "Mean Predicted Conc."))
 }
 
-qp_standards_summary.list <- function(qp) {
-  qp$qp <- qp_standards_summary.data.frame(qp$qp)
-  qp
+#' @export
+qp_standards_summary.list <- function(x) {
+  x$qp <- qp_standards_summary.data.frame(x$qp)
+  x
 }
 
 
 # Samples -----
 ## All ----
-qp_samples_all <- function(qp) {
+qp_samples_all <- function(x) {
   UseMethod("qp_samples_all")
 }
 
-qp_samples_all.data.frame <- function(qp) {
-  dplyr::filter(qp, .data$sample_type == "unknown")
+#' @export
+qp_samples_all.data.frame <- function(x) {
+  dplyr::filter(x, .data$sample_type == "unknown") |>
+    dplyr::select(
+      Name = ".sample_name",
+      Absorbance = ".abs",
+      "Predicted Conc." = ".pred_conc",
+      "Mean Predicted Conc." = ".pred_conc_mean",
+      Outlier = ".is_outlier",
+      Row = ".row",
+      Col = ".col"
+    )
 }
 
-qp_samples_all.list <- function(qp) {
-  qp$qp <- qp_samples_all.data.frame(qp$qp)
-  qp
+#' @export
+qp_samples_all.list <- function(x) {
+  x$qp <- qp_samples_all.data.frame(x$qp)
+  x
 }
 
 ## Summary ----
-qp_samples_summary <- function(qp) {
+qp_samples_summary <- function(x) {
   UseMethod("qp_samples_summary")
 }
 
-qp_samples_summary.data.frame <- function(qp) {
-
+#' @export
+qp_samples_summary.data.frame <- function(x) {
+  qp_samples_all(x) |>
+    dplyr::summarize(.by = c("Name", "Mean Predicted Conc."))
 }
 
-qp_samples_summary.list <- function(qp) {
-  qp$qp <- qp_samples_summary.data.frame(qp$qp)
-  qp
+#' @export
+qp_samples_summary.list <- function(x) {
+  x$qp <- qp_samples_summary.data.frame(x$qp)
+  x
+}
+
+make_well_circle <- function(abs) {
+
 }
