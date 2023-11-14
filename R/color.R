bf02d96115abe50b3293711249e8cbb7d1a54cc4
Kai Aragaki <adamaragaki@gmail.com>
2023-11-14 09:13:05 -0500

    fix: update color scale to use logistic

--- R/color.R
+++ R/color.R
@@ -1,6 +1,6 @@
 qp_pal <- grDevices::colorRampPalette(
-  c("darkseagreen1", "#B7C7B5", "#A763A0", "#3D1452"),
-  bias = 1.5
+  c("#a8ff9f", "#9e67ab", "#975ab6", "#3D1452"),
+  bias = 0.5
 )(100)
 
 # Notes:
@@ -10,9 +10,18 @@ qp_pal <- grDevices::colorRampPalette(
 # Let's call 0.5 the max since you shouldn't go too far above anyway
 
 abs_to_col <- function(abs, pal) {
-  scaled <- abs * 200
-  idx <- ifelse(scaled < 1, 1, scaled)
-  idx <- ifelse(idx > 100, 100, idx)
-  idx <- round(idx)
+  abs <- abs - 0.07
+  scaled <- 100 / (1 + exp(-10 * (abs - 0.15)))
+  idx <- round(scaled)
   pal[idx]
+}
+
+color_test <- function(pal) {
+  tibble::tibble(
+    abs = seq(from = 0, to = 1, by = 0.025),
+    col = abs_to_col(abs, pal)
+  ) |>
+    ggplot2::ggplot(ggplot2::aes(abs, y = 1, color = col)) +
+    ggplot2::geom_point(size = 5) +
+    ggplot2::scale_color_identity()
 }
